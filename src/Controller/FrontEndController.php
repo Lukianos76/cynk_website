7400cf1230155e259962114094f126b4aabb8d81
lukianos76 <lukianos76@gmail.com>
2019-09-02 21:36:11 +0200

    Fix du contact + changement d'images

--- src/Controller/FrontEndController.php
+++ src/Controller/FrontEndController.php
@@ -62,7 +62,7 @@ class FrontEndController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $notification->notify($contact);
             $this->addFlash('success', 'Votre email a bien été envoyé');
-            return $this->redirectToRoute('contact', [
+            return $this->render('contact.html.twig', [
                 'form' => $form->createView()
             ]);
         }
